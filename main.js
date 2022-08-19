c338d081e7857f9977a952dc64a94e08616b9e31
yusupovbekmurod <bekmurodyusupov81@gmail.com>
2022-08-19 20:21:41 +0500

    4-homework_java

---
+++ main.js
@@ -0,0 +1,141 @@
+"use script"
+
+//  ============= 1-misol  ============= 
+
+// let x =+prompt("Son kiriting: ");
+
+// function karra(x){
+//    return Math.pow(x,2);
+  
+// }
+// console.log(karra(x))
+// karra(x)
+
+
+
+//  ============= 2-misol  ============= 
+
+// let num1 =+prompt("Son1 kiriting: ");
+// let num2 =+prompt("Son2 kiriting: ");
+
+// function plus(num1,num2){
+//    return num1 + num2
+
+// }
+// console.log(plus(num1,num2));
+
+//  ============= 3-misol  ============= 
+
+// let num1 =+prompt("Son1 kiriting: ");
+// let num2 =+prompt("Son2 kiriting: ");
+
+// function plus(num1,num2){
+//    return num1 - num2
+
+// }
+// console.log(plus(num1,num2));
+
+//  ============= 4-misol  ============= 
+
+// let x =+prompt("Son1 kiriting: ");
+
+
+// function karra(x){
+//    return Math.pow(x,2);
+  
+// }
+// console.log(karra(x))
+// karra(x)
+
+// function arra(x){
+//    return Math.pow(x,3);
+  
+// }
+// console.log(arra(x))
+// arra(x)
+
+// function darra(x){
+//    return Math.pow(x,4);
+  
+// }
+// console.log(darra(x))
+// darra(x)
+
+//  ============= 5-misol  ============= 
+
+// let a = +prompt("Son kiriting: ");
+
+// function toq(a) {
+//   for (let i = 1; i <= a; i++) {
+//     i % 2 == 1 ? console.log(i) : console.log();
+//   }
+// }
+// toq(a)
+
+//  ============= 6-misol  ============= 
+
+// let a = +prompt("Son kiriting: ");
+
+// function juft(a) {
+//   for (let i = 1; i <= a; i++) {
+//     i % 2 == 0 ? console.log(i) : console.log();
+//   }
+// }
+// juft(a)
+
+//  ============= 7-misol  ============= 
+
+// let n=+prompt("Son kiriting: ");
+
+// function increment (incrementNumber , decrementNumber){
+//    let inc = ++incrementNumber;
+//    let dec = -- decrementNumber;
+
+//    return ` ${inc} , ${dec}`
+// }
+
+// console.log(increment(n,n))
+
+//  ============= 8-misol  ============= 
+
+// let x =+prompt("Son kiriting: ");
+
+// function son(x){
+
+// for(let i=1; i<=x; i++){
+//    x % i == 0 ? console.log(i) : console.log()
+// }
+
+// }
+// son(x)
+
+//  ============= 9-misol  ============= 
+
+// let a =+prompt("Son kiriting: ");
+// let b =+prompt("Son kiriting: ");
+// let x =prompt("Amal kiriting (*,/,+,-): ");
+
+
+// function amal(x){
+//    if(x==="*"){
+//       console.log(a*b)
+//    }else if(x==="/"){
+//       console.log(a/b)
+//    }else if(x=="-"){
+//       console.log(a-b)
+//    }else{
+//       console.log(a+b)
+//    }
+// }
+// amal(x)
+
+// let gacha = 10
+
+// function karakara(gacha){
+//    for(let i=1; i<=gacha; i++){
+//       for(let j=1; j<=gacha; j++){
+//          console.log(i+"*"+j+"="+i*j);
+//       }
+//    }
+// }
+// karakara(gacha)
